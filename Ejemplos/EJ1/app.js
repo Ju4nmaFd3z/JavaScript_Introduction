4dc2d1a20cb5df19bea935473c1d61cbb408c84a
Juan Manuel Fernández Rodríguez <jferrod0510@g.educaand.es>
2026-01-19 11:03:34 +0100

    Update

---
+++ Ejemplos/EJ1/app.js
@@ -0,0 +1,45 @@
+"use strict";
+
+//Variables: let y const
+let edad = 18;
+console.log("Edad:", edad);
+edad = 19;
+console.log("Nueva edad:", edad);
+
+const centro = "CPIFP Alan Turing";
+console.log("Centro:", centro);
+
+//Operadores
+const a = 10;
+const b = 3;
+console.log("Suma: ", a + b);
+console.log("Resta: ", a - b);
+console.log("Multiplicación: ", a * b);
+console.log("División: ", a / b);
+console.log("Resto: ", a % b);
+
+const nombre = "Compi";
+console.log("Hola " + nombre + "!")
+console.log(`Hola ${nombre}!`)
+
+const firstWord = "frase";
+const secondWord = "concatenada";
+
+console.log("Una " + firstWord + " bien " + secondWord); // Ejemplo 1 concatenación
+console.log(`Una ${firstWord} mejor ${secondWord}`); // Ejemplo 2 concatenación
+
+const magicalWord = `<strong>Magical Word</strong>`;
+
+//Ejercicio producto
+
+const producto = {
+    nombre: "Producto 1",
+    precio: 100,
+    stock: 5,
+    toString: function() {
+        return `Producto: ${this.nombre}\nPrecio: ${this.precio}$\nStock: ${this.stock}`;
+    }
+};
+
+//console.log(`Producto: ${producto.nombre}\nPrecio: ${producto.precio}$\nStock: ${producto.stock}`)
+console.log(producto.toString());
