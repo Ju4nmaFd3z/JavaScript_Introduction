05c10d17e81e0c52b33eed6df972acc1559faa88
Juan Manuel Fernández Rodríguez <juanmafr2007@gmail.com>
2026-02-01 15:47:07 +0100

    Update

---
+++ Ejercicios/Relación_01/app.js
@@ -0,0 +1,35 @@
+//Ejercicio 01
+
+let nombre = "Juan Manuel Fernández Rodríguez";
+const edad = 18;
+
+document.getElementById('pEJ01').innerText = `Mi nombre es ${nombre} y tengo ${edad} años`;
+
+//Ejercicio 02
+
+const a = 12;
+const b = 5;
+
+const suma = a+b;
+const resta = a-b;
+const producto = a*b;
+const division = a/b;
+const resto = a%b;
+
+//Ejercicio 03
+
+//Ejercicio 04
+
+//Ejercicio 05
+
+//Ejercicio 06
+
+//Ejercicio 07
+
+//Ejercicio 08
+
+//Ejercicio 09
+
+//Ejercicio 10
+
+//...
